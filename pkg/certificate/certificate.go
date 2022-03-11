fc8b8fa43b97d96fac3504aa80a7f4347e7de715
Stefan Majewsky <stefan.majewsky@sap.com>
2022-03-11 16:13:39 +0100

    add Vault support

--- pkg/certificate/certificate.go
+++ pkg/certificate/certificate.go
@@ -1,7 +1,9 @@
 package certificate
 
 import (
+	"bytes"
 	"fmt"
+	"html/template"
 	"os"
 	"path/filepath"
 	"strings"
@@ -13,6 +15,7 @@ type Certificate struct {
 	CommonName string   `yaml:"cn" json:"cn"`
 	SANS       []string `yaml:"sans,omitempty" json:"sans,omitempty"`
 	OutFolder  string   `yaml:"-" json:"-"`
+	VaultPath  string   `yaml:"-" json:"-"`
 }
 
 func (c *Certificate) GetName() string {
@@ -32,6 +35,9 @@ func ReadCertificateConfig(filePath string) ([]*Certificate, error) {
 	}
 
 	type certCfg struct {
+		Vault struct {
+			PathTemplate string `yaml:"path"`
+		} `yaml:"vault"`
 		Certificates []*Certificate `yaml:"certificates" json:"certificates"`
 	}
 
@@ -40,13 +46,28 @@ func ReadCertificateConfig(filePath string) ([]*Certificate, error) {
 		return nil, err
 	}
 
+	vaultPathTpl, err := template.New(filePath + "/vault.path").Parse(c.Vault.PathTemplate)
+	if err != nil {
+		return nil, err
+	}
+
 	certs := c.Certificates
 	for idx, c := range certs {
 		// Ensure the common name is part of the SANs.
 		certs[idx].SANS = checkSANs(c.CommonName, c.SANS)
 
-		// Remember where to store the certificate and key.
+		// Remember where to store the certificate and key in Git.
 		certs[idx].OutFolder = filepath.Dir(filePath)
+
+		// Calculate where to store the certificate and key in Vault.
+		var buf bytes.Buffer
+		err = vaultPathTpl.Execute(&buf, map[string]interface{}{
+			"PathSafeCommonName": strings.ReplaceAll(c.CommonName, "*", "wildcard"),
+		})
+		if err != nil {
+			return nil, fmt.Errorf("while evaluating vault.path template for %q: %w", c.CommonName, err)
+		}
+		certs[idx].VaultPath = buf.String()
 	}
 
 	return certs, nil
